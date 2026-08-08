a0cf826591dc4e9b4400d4f12b7fab2433d292ab
Jerome Ansia <jerome.ans@gmail.com>
2026-08-08 11:50:12 -0700

    chore(android): versionName 0.9.7 for inbox fix OTA (code 36)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -37,7 +37,7 @@ android {
         minSdk = 26
         targetSdk = 35
         versionCode = 36
-        versionName = "0.9.5"
+        versionName = "0.9.7"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
         buildConfigField(
